c941b25883bf0f27772b0ec3939139616fa7a780
ria patel <ria.amy.patel@gmail.com>
2020-01-08 17:20:59 +0000

    added redirect

--- app.rb
+++ app.rb
@@ -7,13 +7,11 @@ class Battle < Sinatra::Base
     erb :index
   end
 
-  run! if app_file == $0
-
   post '/names' do
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
     # erb :play
-    redirect '/play'
+    redirect to('/play')
   end
 
   get '/play' do
@@ -21,4 +19,6 @@ class Battle < Sinatra::Base
     @player_2 = session[:player_2]
     erb :play
   end
+
+  run! if app_file == $0
 end
