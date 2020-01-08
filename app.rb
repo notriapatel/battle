7de011f98e494ff97b0ee58aa4a82a2e06b079da
ria patel <ria.amy.patel@gmail.com>
2020-01-08 16:55:55 +0000

    working on redirect

--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do 
-    # 'Testing infrastructure working!'
     erb :index
   end
 
   run! if app_file == $0
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    # erb :play
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 end
