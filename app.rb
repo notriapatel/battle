be214bc2e503ef688341cb5a69492fee14f1fc5b
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-08 14:42:19 +0000

    Added feature test

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 
-
 require 'sinatra/base'
 
 
 
 class Battle < Sinatra::Base
   get '/' do 
-    'Hello Battle!'
+    'Testing infrastructure working!'
   end
 
   run! if app_file == $0
 end
+
+
+
 
